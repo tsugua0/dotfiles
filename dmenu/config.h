7c88c6b839c280a313f269bc272a75265b0f373e
tsugua0 <q.tsugua@gmail.com>
2021-08-05 18:15:48 +0300

    add dmenu config

---
+++ dmenu/config.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"xos4 Terminus:size=12"
+};
+static const char *prompt      = "exec";      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ffffce", "#000000" },
+	[SchemeSel] = { "#ffffce", "#734c4d" },
+	[SchemeOut] = { "#000000", "#aa8663" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
